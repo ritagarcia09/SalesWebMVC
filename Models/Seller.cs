c5d88ead7c29f1297dd087e8c0e164e4a03d53cc
Rita Garcia <rita.garcia.0907@gmail.com>
2019-12-28 15:52:32 -0300

    validacao

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -8,19 +8,33 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
         public String Name { get; set; }
+        
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public String Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]       
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage ="{0} mustbe form {1} to {2}")]
         [Display(Name = "Base Salary")]
-        [DisplayFormat(DataFormatString ="{0:F2}")]
+        [DisplayFormat(DataFormatString ="{0:F2}")]   
         public Double BaseSalary { get; set; }
+        
         public Department Department { get; set; }
         [Display(Name = "Department")]
+        
         public int DepartmentId { get; set; }
+        
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
